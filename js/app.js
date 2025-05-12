956f2d69d0e76d8a8979c7b42bc7af0ba9d5d7d9
CalebDH31 <calebdh31@gmail.com>
2025-05-11 22:10:09 -0600

    myStory built out and functions added

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+/*-------------------------------- Constants --------------------------------*/
+const startingScreen = document.querySelector('.starting-screen')
+const startGame = document.querySelector('.start')
+const options = document.querySelector('.option-buttons')
+const allCards = document.querySelectorAll('.story')
+
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice = ''
+let currentCard = 'card-one'
+
+/*------------------------ Cached Element References ------------------------*/
+const option1Btn = document.querySelector('#option1')
+const option2Btn = document.querySelector('#option2')
+const resetBtn = document.querySelector('#reset')
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
